e10355244c4d383dd28c2876b8d4c3f65bd4b6f4
neerajparamkar <neerajparamkar@gmail.com>
2026-09-11 10:53:13 +0530

    updated mobile ui and some components

--- app/impossible-ai/athletes/page.tsx
+++ app/impossible-ai/athletes/page.tsx
@@ -5,6 +5,7 @@ import AthleteNetwork from "@ia/components/sections/AthleteNetwork";
 import Community from "@ia/components/sections/Community";
 import Footer from "@ia/components/Footer";
 
+
 export const metadata: Metadata = {
     description: "India's first performance-based athlete network, plus hyperlocal community and social features.",
 };
